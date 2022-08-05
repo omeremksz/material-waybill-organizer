2a6cde30e85b879e32635203d2605e682f6a8738
omeremksz <omeremksz@gmail.com>
2022-08-05 19:30:28 +0300

    Initial Project Added.

---
+++ material-waybill-organizer/src/main/java/kalemyazilim/materialapp/materialwaybillorganizer/MaterialWaybillOrganizerApplication.java
@@ -0,0 +1,13 @@
+package kalemyazilim.materialapp.materialwaybillorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialWaybillOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialWaybillOrganizerApplication.class, args);
+    }
+
+}
---
+++ material-waybill-organizer/src/test/java/kalemyazilim/materialapp/materialwaybillorganizer/MaterialWaybillOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package kalemyazilim.materialapp.materialwaybillorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialWaybillOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
